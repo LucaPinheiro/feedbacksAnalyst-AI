247675a6f2ae50abe1359c40e5c9afe5834449c7
LucaPinheiro <lucapgomes11@gmail.com>
2024-10-28 22:50:35 -0300

    Refactor feedback controller to handle missing parameters and exceptions

--- app/controllers/feedback_controller.py
+++ app/controllers/feedback_controller.py
@@ -1,5 +1,15 @@
+from app.core.helpers.errors.controller_errors import MissingParameters
+from app.core.helpers.external_interfaces import BadRequest, InternalServerError
 from app.services.sentiment_service import analyze_feedback
 from app.schemas.feedback_schema import FeedbackResponse
 
 def get_feedback_analysis(feedback_text: str) -> FeedbackResponse:
-    return analyze_feedback(feedback_text)
+    try:
+        if not feedback_text:
+            raise MissingParameters("feeedback")
+        return analyze_feedback(feedback_text)
+    
+    except MissingParameters as e:
+      return BadRequest(body=e.message)
+    except Exception as e:
+        return InternalServerError(body=str(e)) 
